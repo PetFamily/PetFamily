1ac052f8b9a470b2fb806cf90a0c0676afa15b03
PomPom248 <sofiroggero@gmail.com>
2018-11-26 10:45:23 +0100

    model

--- models/User.js
+++ models/User.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  availability: { type: String, required: true },
+  pricePerHour: {type: String, required: true },
+ type: { type: String, required: true, enum: ['Adoption', 'Babysitting', 'Both', 'Other'] }
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
