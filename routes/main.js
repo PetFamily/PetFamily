d3f9b0bcbef709ce4b9ccdaf48fb0a629daa5bc4
PomPom248 <sofiroggero@gmail.com>
2018-11-26 20:22:17 +0100

    mods

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,9 @@ const mainRouter = express.Router();
 const User = require("../models/User")
 
 mainRouter.get('/', (req, res, next) => {
-  res.render('main');
+  User.findById(req.params.id).then((user) => {
+    res.render('main', { user });
+  })
 });
 mainRouter.get('/new', (req, res, next) => {
   res.render('userLocation');
@@ -12,13 +14,13 @@ mainRouter.get('/new', (req, res, next) => {
 mainRouter.post('/new', (req, res, next) => {
   User.findByIdAndUpdate(
     { _id: req.params._id },
-    { $set:{address}})
+    { $set: { address } })
   res.redirect('/main');
 });
 
 mainRouter.get('/:id', (req, res, next) => {
-  User.findById(req.params.id).then((user)=>{
-    res.render('user-profile', {user});
+  User.findById(req.params.id).then((user) => {
+    res.render('user-profile', { user });
 
   })
 });
--- routes/profile.js
+++ routes/profile.js
@@ -7,24 +7,25 @@ profileRouter.get('/', (req, res, next) => {
 });
 
 profileRouter.post('/', (req, res, next) => {
-const {availability, pricePerHour, description, typeActivity} = req.body;
+  const { availability, pricePerHour, description, typeActivity } = req.body;
 
-const newProfile = new User({availability, pricePerHour, description, typeActivity})
-User.findByIdAndUpdate({_id: req.user._id}, {$set: {availability, pricePerHour, description, typeActivity}})
+  const newProfile = new User({ availability, pricePerHour, description, typeActivity })
+  User.findByIdAndUpdate({ _id: req.user._id }, { $set: { availability, pricePerHour, description, typeActivity } })
 
-.then((user)=>{
-console.log("User added properly")
-res.redirect('main');
-})
-.catch(error =>{
-  console.log("User NOT ADDED");
-  })
+    .then((user) => {
+      console.log("User added properly")
+      res.redirect('main', { user });
+    })
+    .catch(error => {
+      console.log("User NOT ADDED");
+    })
 });
 
 profileRouter.get('/:id', (req, res, next) => {
-  User.findById(req.params.id).then((user)=>{
-    res.render('user-profile',{user});
-  })
+  User.findById(req.params._id)
+    .then((user) => {
+      res.render('user-profile', { user });
+    })
 
 });
 
