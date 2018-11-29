5673b2931f4ae0ec5955adbcae3e3aafc3bd22e5
PomPom248 <sofiroggero@gmail.com>
2018-11-29 17:38:39 +0100

    modification in model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  userType: { type: String, enum: ["Adoption Center", "Individual"] },
+  userType: { type: String, enum: ["Adoption Center", "Citizen"] },
   profilePic: { type: String },
   profilePath: String,
   availability: { type: String },
