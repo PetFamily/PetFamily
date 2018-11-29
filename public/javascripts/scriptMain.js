526908e1bffd4f5cfecb5ac61e262da91e2c418f
aterron84 <aterron84@gmail.com>
2018-11-29 11:51:21 +0100

    css in map changes

--- public/javascripts/scriptMain.js
+++ public/javascripts/scriptMain.js
@@ -31,8 +31,8 @@ function startMap() {
             url: icon
           }
         });
-        // addMarker(user_location.lat, user_location.lng, "España", 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-        // addMarker(window.pepe.address.address.lat, window.pepe.address.address.lng, window.pepe.address.userLocationName, "http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
+         addMarker(user_location.lat, user_location.lng, "España", 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+         addMarker(window.pepe.address.address.lat, window.pepe.address.address.lng, window.pepe.address.userLocationName, "http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
         
         // const userLocationName = req.User.userLocationName ;
         // const address = req.User.address;
