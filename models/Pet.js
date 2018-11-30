9c6a78701b6b90951a7724194f720c65cc995e33
PomPom248 <sofiroggero@gmail.com>
2018-11-30 10:16:53 +0100

    modifications in some fields

--- models/Pet.js
+++ models/Pet.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const petSchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   ownerID: { type: Schema.Types.ObjectId, ref: 'User' },
   type: {
     type: String, required: true, enum: ["Dog", "Cat", "Other"]
   },
-  description: { type: String},
+  description: { type: String },
   vaccunationCompleted: Boolean,
   age: { type: String },
-  notes: { type: String},
+  notes: { type: String },
   petPhoto: { type: String },
   petPath: { type: String }
 });
